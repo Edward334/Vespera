ee68bec8a02ef80f3faa50f2a3aa443412a67aec
asterveil <mouwanzz114514@gmail.com>
2026-08-11 21:59:30 +0800

    fix: move shared compose entrypoint

--- composeApp/src/commonMain/kotlin/dev/vespera/player/AppEntry.kt
+++ composeApp/src/commonMain/kotlin/dev/vespera/player/AppEntry.kt
@@ -1,7 +1,8 @@
 package dev.vespera.player
 
 import androidx.compose.runtime.Composable
-import dev.vespera.player.ui.VesperaApp
 import dev.vespera.player.data.createMusicApi
+import dev.vespera.player.ui.VesperaApp
 
-@Composable fun App() = VesperaApp(createMusicApi())
+@Composable
+fun App() = VesperaApp(createMusicApi())
